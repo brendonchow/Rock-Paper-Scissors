3dcecb332fbbb7a733175794b33b6ce1bb6ef43e
Brendon Chow <brendonchow123@gmail.com>
2022-11-26 14:41:06 +1100

    Misc chaanges

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-Generate random number from 0-2
-Return Rock if number is 0
-Return Paper if number is 1
-Return Scissors if number is 2
-*/
 function getComputerChoice()
 {
     randomNatural = Math.floor(Math.random() * 3);
@@ -11,15 +5,7 @@ function getComputerChoice()
             (randomNatural === 1) ? "Paper" : "Scissors";
 }
 
-/*
-Takes in player's selection X and computer selection Y
-Compare selections case insensitively
-If player wins: return "You win! X beats Y"
-If player loses: return "You lose! Y beats X"
-If same selection: return "Both of you have chosen X!"
-*/
-
-function playRound(player, computer)
+function isWinner(player, computer)
 {
     if (player === computer)
     {
@@ -33,22 +19,24 @@ function playRound(player, computer)
     {
         return (computer === "Rock") ? 1 : -1;
     }
-    return (computer === "Paper") ? 1 : -1;
+    if (player == "Scissors")
+    {
+        return (computer === "Paper") ? 1 : -1;
+    }
 }
 
-/* Player and computer play a round of 5
-Print the result of each round and determine the ultimate winner*/
 function game()
 {
     let playerWins = 0;
     let computerWins = 0;
     let ties = 0;
+    // Play 5 rounds
     for (let i = 1; i <= 5; i++)
     {
         let capitalize = (s) => s[0].toUpperCase() + s.substr(1).toLowerCase();
         let player = capitalize(prompt("Enter 'Rock', 'Paper' or 'Scissors'."));
         let computer = getComputerChoice();
-        let result = playRound(player,  computer);
+        let result = isWinner(player,  computer);
 
         let outputResult;
         if (result === 0)
@@ -66,13 +54,14 @@ function game()
             outputResult = "You lose!";
             computerWins++;
         }
-        console.log(`Round ${i}: ${outputResult} You chose "${player}" and the computer chose "${computer}". `);
+        console.log(`Round ${i}: ${outputResult} You chose "${player}" and the computer chose "${computer}".`);
     }
 
     let result =(playerWins > computerWins) ? "You are the final winner. Congratulations!" :
                 (playerWins < computerWins) ? "The computer is the final winner. Better luck next time!" :
                 "It's a tie!";
-    console.log(`You won ${playerWins} times, the computer won ${computerWins} times and there were ${ties} ties! ${result}`);
+    // Print results and stats on their own lines
+    console.log(`${result}\nWins: ${playerWins}\nLoses: ${computerWins}\nTies: ${ties}`);
 }
 
 game();
