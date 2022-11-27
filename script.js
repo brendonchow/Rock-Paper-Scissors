37ffe4fcd6bd76ca10aac5bc7d73fbb9597318dd
Brendon Chow <brendonchow123@gmail.com>
2022-11-27 20:31:03 +1100

    Misc changes

--- script.js
+++ script.js
@@ -4,32 +4,11 @@ const trackPlayer = document.querySelector(".playerScore");
 const trackComputer = document.querySelector(".computerScore");
 const message = document.querySelector(".message");
 const buttons = document.querySelectorAll(".buttons button");
-const playButton = document.querySelector(".play"); 
+const playButton = document.querySelector(".play");
+const endGameMessage = document.querySelector(".endgameMessage");
+const endGame = document.querySelector(".endgame")
 
 message.textContent = INITIAL_QUOTE;
-
-playButton.addEventListener("mousedown", () => {
-    trackComputer.textContent = 0;
-    trackPlayer.textContent = 0;
-    playButton.classList.toggle("clickPlay")
-    message.textContent = INITIAL_QUOTE;
-})
-
-buttons.forEach(button => {
-    button.addEventListener("click", e => {
-        playRound(e.target.classList.value, getComputerChoice());
-    });
-
-});
-
-message.addEventListener("transitionend", e => e.target.classList.remove("messageChange"));
-
-window.addEventListener("keydown", (e) => {
-    const player = document.querySelector(`.${e.key}`);
-    if (!player) return;
-    playRound(player.classList.value.slice(0, player.classList.value.length - 2), getComputerChoice());
-    console.log(player.classList.value);
-})
 
 function getComputerChoice()
 {
@@ -40,30 +19,43 @@ function getComputerChoice()
 function playRound(player, computer)
 {
     if (trackPlayer.textContent == 5 || trackComputer.textContent == 5) return;
-    message.classList.add("messageChange");
-    if (player === computer) message.textContent = "Tie!";
-    else if (player === "Rock") determineResult(computer, "Scissors");
-    else if (player === "Paper") determineResult(computer, "Rock");
-    else determineResult(computer, "Paper");
+    if (player === computer) message.textContent = `Tie! You both chose ${player}!`;
+    else if (player === "Rock") determineResult(player, computer, "Scissors");
+    else if (player === "Paper") determineResult(player, computer, "Rock");
+    else determineResult(player, computer, "Paper");
 }
 
-function determineResult(computer, playerWin)
+function determineResult(player, computer, playerWin)
 {
     if (computer === playerWin) 
     {
         trackPlayer.textContent++;
-        message.textContent = "Player + 1"
+        message.textContent = `You win! ${player} beats ${computer}!`;
     } 
     else 
     {
         trackComputer.textContent++;
-        message.textContent = "Computer + 1"
+        message.textContent = `You lose! ${computer} beats ${player}!`;
     }
     if (trackPlayer.textContent == 5 || trackComputer.textContent == 5)
     {
-        if (trackPlayer.textContent == 5) message.textContent = "Player Wins!";
-        else message.textContent = "Computer Wins!";
-        playButton.classList.toggle("clickPlay");
+        if (trackPlayer.textContent == 5) endGameMessage.textContent = "Congratulations! You are the ultimate winner!";
+        else endGameMessage.textContent = "The computer won! Better luck next time!";
+        endGame.classList.toggle("clickPlay");
 
     }
 }
+
+playButton.addEventListener("mousedown", () => {
+    trackComputer.textContent = 0;
+    trackPlayer.textContent = 0;
+    endGame.classList.toggle("clickPlay")
+    message.textContent = INITIAL_QUOTE;
+})
+
+buttons.forEach(button => {
+    button.addEventListener("click", e => {
+        playRound(e.target.classList.value, getComputerChoice());
+        console.log(e.target.classList.value)
+    });
+});
